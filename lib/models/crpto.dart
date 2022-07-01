fe192ccc84024c3547a364bd4ae9727dde73b6fd
godsakani <nyonggodwill11@gmail.com>
2022-07-01 12:43:00 +0100

    feat: added models for crypto

---
+++ lib/models/crpto.dart
@@ -0,0 +1,26 @@
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'JPY',
+  'USD',
+  'ZAR',
+  'RUB',
+  'NZD'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC'
+];
+ 
+class cryptoData {
+
+}
