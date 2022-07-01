d45af22eae87648b949f45a471679c559ff677b9
godsakani <nyonggodwill11@gmail.com>
2022-07-01 12:40:02 +0100

    feat: added dropdown navigation added IOS and Android dropdown

--- lib/Screens/crypto_screen.dart
+++ lib/Screens/crypto_screen.dart
@@ -1,15 +1,62 @@
+import 'package:crypto/models/crpto.dart';
 import 'package:crypto/utilities/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:crypto/components/card_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 class CryptoScreen extends StatefulWidget {
   const CryptoScreen({Key? key}) : super(key: key);
-
   @override
   State<CryptoScreen> createState() => _CryptoScreenState();
 }
 
 class _CryptoScreenState extends State<CryptoScreen> {
+  String selectedCurrency = 'USD';
+
+  DropdownButton<String> androidDropdown() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropdownItems.add(newItem);
+    }
+    return DropdownButton(
+      value: selectedCurrency,
+      iconEnabledColor: Colors.white,
+      dropdownColor: Colors.black45,
+      style: kBottomsheetText,
+      items: dropdownItems,
+      onChanged: (value) {
+        setState(() {
+          selectedCurrency = value.toString();
+        });
+      },
+    );
+  }
+
+  CupertinoPicker IOSPicker() {
+    List<Text> pickerItem = [];
+    for (String currency in currenciesList) {
+      Text(currency);
+      pickerItem.add(Text(
+        currency,
+        style: kBottomsheetText,
+      ));
+    }
+
+    return CupertinoPicker(
+      backgroundColor: Colors.blue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        print(selectedIndex);
+      },
+      children: pickerItem,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,12 +114,9 @@ class _CryptoScreenState extends State<CryptoScreen> {
             child: Container(
               height: 200,
               color: Colors.blue,
-              child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'BTC',
-                    style: kBottomsheetText,
-                  )),
+              child: Center(
+                child: Platform.isIOS ? IOSPicker() : androidDropdown(),
+              ),
             ),
           ),
         ],
