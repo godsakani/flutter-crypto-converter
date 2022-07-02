febfdb396e90280b6691f2354b809abe536ade4b
godsakani <nyonggodwill11@gmail.com>
2022-07-02 17:17:12 +0100

    feat:modified file

--- lib/models/crpto.dart
+++ lib/models/crpto.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -8,19 +11,42 @@ const List<String> currenciesList = [
   'HKD',
   'IDR',
   'ILS',
+  'INR',
   'JPY',
-  'USD',
-  'ZAR',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
   'RUB',
-  'NZD'
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
 ];
 
-const List<String> cryptoList = [
-  'BTC',
-  'ETH',
-  'LTC'
-];
- 
-class cryptoData {
+const List<String> cryptoList = ['BTC', 'ETH', 'LTC'];
 
+const apiKey = '';
+const coinURL = 'https://rest.coinapi.io/v1/exchangerate';
+
+class CoinModel {
+  Future getCoinData(String selectCoin) async {
+    Map<String, String> cryptoPrice = {};
+    for( String crypto in cryptoList) {
+    String url = '$coinURL$crypto$selectCoin?apikey=$apiKey';
+    Response response = await get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+
+      throw 'Problem with the get request';
+    }
+    }
+    return cryptoPrice;
+  }
 }
